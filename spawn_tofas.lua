04c066d6731d0c03a9faaf4d86db17516c389a4d
WShadowStudios <129547469+WShadowStudios@users.noreply.github.com>
2025-02-03 19:30:34 +0300

    Update spawn_tofas.lua

--- spawn_tofas.lua
+++ spawn_tofas.lua
@@ -3,11 +3,15 @@
 --local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 
 ---------------------------------------------------------------------------------------------------------------------------
+
+local Players = game:GetService("Players")
+local lplr = Players.LocalPlayer
+
 local tool = Instance.new("Tool")
 tool.Name = "TofasSpawner"
 tool.TextureId = "rbxassetid://2328243272"
 tool.RequiresHandle = false
-tool.Parent = game.Players.LocalPlayer.Backpack
+tool.Parent = lplr.Backpack
 
 function spawn()
 local tofas = LoadCustomInstance("https://raw.githubusercontent.com/WShadowStudios/scripts/main/Tofas.rbxm")
