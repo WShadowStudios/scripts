46d45fb53dfbc852cbf0b94f34f670cc42e11497
WShadowStudios <129547469+WShadowStudios@users.noreply.github.com>
2025-02-03 18:58:20 +0300

    normal_tablet.lua

--- normal_tablet.lua
+++ normal_tablet.lua
@@ -3,5 +3,7 @@
 --local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 
 ---------------------------------------------------------------------------------------------------------------------------
-local Scanner = LoadCustomInstance("https://raw.githubusercontent.com/WShadowStudios/tablet/main/original_tablet.rbxm")
-Scanner.Parent = game.Players.LocalPlayer.Backpack
+local tool = Instance.new("Tool")
+tool.RequiresHandle = false
+
+Tool.Parent = game.Players.LocalPlayer.Backpack
